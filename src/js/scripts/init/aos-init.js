ecc601e5f000d0a4cb09ecfc6f3cc750c8fd7bf5
kiforks <ks.kifor@gmail.com>
2020-09-08 00:29:39 +0300

    Finish

--- src/js/scripts/init/aos-init.js
+++ src/js/scripts/init/aos-init.js
@@ -1,11 +1,11 @@
 if (document.documentElement.clientWidth < 1199) {
   AOS.init({
-    duration: 2000,
+    duration: 1500,
     offset: 0
   })
 } else {
   AOS.init({
-    duration: 2000,
+    duration: 1500,
     offset: 100
   })
 }
